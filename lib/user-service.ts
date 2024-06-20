cb151e4696edd219e5991ee21e47cec7c309f96a
NSerus <nunoworkingserus@gmail.com>
2024-06-20 17:27:08 +0100

    created getUserById

--- lib/user-service.ts
+++ lib/user-service.ts
@@ -5,6 +5,19 @@ export async function getUserByUsername(username: string) {
     where: {
       username,
     },
+    include: {
+      stream: true,
+    },
   });
   return user;
 }
+
+export const getUserById = async (id: string) => {
+  const user = await db.user.findUnique({
+    where: { id },
+    include: {
+      stream: true,
+    },
+  });
+  return true;
+};
