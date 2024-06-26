9080f801ca4693a9247247791ace68b96dc5de37
NSerus <nunoworkingserus@gmail.com>
2024-06-26 02:16:35 +0100

    chat sidebar structure and passing props

---
+++ components/stream-player/chat.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+interface ChatProps {
+  hostName: string;
+  hostIdentity: string;
+  viewerName: string;
+  isFollowing: boolean;
+  isChatEnabled: boolean;
+  isChatDelayed: boolean;
+  isChatFollowersOnly: boolean;
+}
+
+export const Chat = ({
+  hostName,
+  hostIdentity,
+  viewerName,
+  isFollowing,
+  isChatEnabled,
+  isChatDelayed,
+  isChatFollowersOnly,
+}: ChatProps) => {
+  return <div>Chats</div>;
+};
