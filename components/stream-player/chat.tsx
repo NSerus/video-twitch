12418ffabd948dc70a0e1cb63663217b3375fed8
NSerus <nunoworkingserus@gmail.com>
2024-06-26 11:20:18 +0100

    Added chat form FE

--- components/stream-player/chat.tsx
+++ components/stream-player/chat.tsx
@@ -8,9 +8,10 @@ import {
   useRemoteParticipant,
 } from "@livekit/components-react";
 
-import { useChatSidebar } from "@/store/use-chat-sidebar";
+import { ChatVariant, useChatSidebar } from "@/store/use-chat-sidebar";
 import { useEffect, useMemo, useState } from "react";
 import { ChatHeader } from "./chat-header";
+import { ChatForm } from "./chat-form";
 
 interface ChatProps {
   hostName: string;
@@ -68,6 +69,24 @@ export const Chat = ({
   return (
     <div className="flex flex-col bg-background border-l border-b pt-0 h-[calc(100vh-80px)]">
       <ChatHeader />
+      {variant === ChatVariant.CHAT && (
+        <>
+          <ChatForm
+            onSubmit={onSubmit}
+            value={value}
+            onChange={onChange}
+            isHidden={isHidden}
+            isFollowersOnly={isChatFollowersOnly}
+            isDelayed={isChatDelayed}
+            isFollowing={isFollowing}
+          />
+        </>
+      )}
+      {variant === ChatVariant.COMMUNITY && (
+        <>
+          <p>Commmunity mode</p>
+        </>
+      )}
     </div>
   );
 };
