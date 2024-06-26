788457f956fc6049c64e2bc680aa53a572c523eb
NSerus <nunoworkingserus@gmail.com>
2024-06-26 11:32:02 +0100

    Handling submissions and hidding chat

--- components/stream-player/chat-form.tsx
+++ components/stream-player/chat-form.tsx
@@ -26,25 +26,66 @@ export const ChatForm = ({
   isFollowersOnly,
   isDelayed,
 }: ChatFormProps) => {
+  const [isDelayBlocked, setIsDelayBlocked] = useState(false);
+
+  const isFollowersOnlyAndNotFollowing = isFollowersOnly && !isFollowing;
+  const isDisabled =
+    isHidden || isDelayBlocked || isFollowersOnlyAndNotFollowing;
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    if (!value || isDisabled) return;
+
+    //Delay timeout
+    if (isDelayed && !isDelayBlocked) {
+      setIsDelayBlocked(true);
+      setTimeout(() => {
+        setIsDelayBlocked(false);
+        onSubmit();
+      }, 3000);
+    } else {
+      onSubmit();
+    }
+  };
+
+  if (isHidden) return null;
+
   return (
     <form
-      onSubmit={() => {}}
+      onSubmit={handleSubmit}
       className="flex flex-col items-center gap-y-4 p-3"
     >
       <div className="w-full">
         <Input
-          onChange={() => {}}
+          onChange={(e) => onChange(e.target.value)}
           value={value}
-          disabled={false}
+          disabled={isDisabled}
           placeholder="Send a Message"
-          className={cn("border-white/10")}
+          className={cn(
+            "border-white/10",
+            isFollowersOnly && " rounded-t-none border-t-0"
+          )}
         />
       </div>
       <div className="ml-auto">
-        <Button type="submit" variant="primary" size="sm" disabled={false}>
+        <Button type="submit" variant="primary" size="sm" disabled={isDisabled}>
           Chat
         </Button>
       </div>
     </form>
+  );
+};
+
+export const ChatFormSkeleton = () => {
+  return (
+    <div className="flex flex-col items-center gap-y-4 p-3">
+      <Skeleton className="w-full h-10" />
+      <div className="flex items-center gap-x-2 ml-auto">
+        <Skeleton className="h-7 w-7" />
+        <Skeleton className="h-7 w-12" />
+      </div>
+    </div>
   );
 };
