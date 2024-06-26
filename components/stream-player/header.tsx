71715765e45b37c1866780e87c24151b605461b4
NSerus <nunoworkingserus@gmail.com>
2024-06-26 18:52:57 +0100

    Header with participant counter

---
+++ components/stream-player/header.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import { UserIcon } from "lucide-react";
+
+import {
+  useParticipants,
+  useRemoteParticipant,
+} from "@livekit/components-react";
+import { UserAvatar } from "../user-avatar";
+import { VerifiedMark } from "../verified-mark";
+
+interface HeaderProps {
+  imageUrl: string;
+  hostName: string;
+  hostIdentity: string;
+  viewerIdentity: string;
+  isFollowing: boolean;
+  name: string;
+}
+
+export const Header = ({
+  hostName,
+  imageUrl,
+  hostIdentity,
+  viewerIdentity,
+  isFollowing,
+  name,
+}: HeaderProps) => {
+  const participants = useParticipants();
+  const participant = useRemoteParticipant(hostIdentity);
+
+  const isLive = !!participant;
+  const participantCount = participants.length - 1;
+
+  const hostAsViewer = `host-${hostIdentity}`;
+  const isHost = viewerIdentity === hostAsViewer;
+
+  return (
+    <div className=" flex flex-col-1 lg:flex-row gap-y-4 lg:gap-y-0 items-start justify-between px-4">
+      <div className="flex items-center gap-x-3">
+        <UserAvatar
+          imageUrl={imageUrl}
+          username={hostName}
+          size={"lg"}
+          isLive={true}
+          showBadge
+        />
+        <div className="space-y-1">
+          <div className="flex items-center gap-x-2">
+            <h2 className="text-lg font-semibold">{hostName}</h2>
+            <VerifiedMark />
+          </div>
+          <p className="text-sm font-semibold">{name}</p>
+          {isLive ? (
+            <div className="font-semibold flex gap-x-1 items-center text-xs text-rose-500">
+              <UserIcon className="" />
+              <p>
+                {participantCount}{" "}
+                {participantCount === 1 ? "viewer" : "viewers"}
+              </p>
+            </div>
+          ) : (
+            <p className="font-semibold text-xs text-muted-foreground">
+              Offline
+            </p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
