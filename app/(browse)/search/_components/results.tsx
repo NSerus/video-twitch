e556688ed53868ed7dc8a6e7c0d349582e8dae63
NSerus <nunoworkingserus@gmail.com>
2024-06-30 22:53:37 +0100

    Search page

---
+++ app/(browse)/search/_components/results.tsx
@@ -0,0 +1,32 @@
+import { getSearch } from "@/lib/search-sevice";
+import { ResultCard } from "./result-card";
+
+interface ResultsProps {
+  term?: string;
+}
+
+export const Results = async ({ term }: ResultsProps) => {
+  const data = await getSearch(term);
+
+  return (
+    <div>
+      <h2 className="text-lg font-semibold mb-4">
+        Results for term &quot;{term}&quot;
+      </h2>
+      {data.length === 0 && (
+        <p className="text-muted-foreground text-sm">
+          No results found. Try searching for something else
+        </p>
+      )}
+      <div className="flex flex-col gap-y-4">
+        {data.map((result) => (
+          <ResultCard data={result} key={result.id} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export const ResultsSkeleton = () => {
+  return <div></div>;
+};
