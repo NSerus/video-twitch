268340608b33392a32989f981313f6aa4de460de
NSerus <nunoworkingserus@gmail.com>
2024-06-30 23:51:11 +0100

    Added data to community tab

---
+++ app/(dashboard)/u/[username]/community/_components/columns.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { UserAvatar } from "@/components/user-avatar";
+import { ColumnDef } from "@tanstack/react-table";
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+export type BlockedUser = {
+  id: string;
+  userId: string;
+  imageUrl: string;
+  username: string;
+  createdAt: string;
+};
+
+export const columns: ColumnDef<BlockedUser>[] = [
+  {
+    accessorKey: "username",
+    header: "Username",
+    cell: ({ row }) => (
+      <div className="flex items-center gap-x-4">
+        <UserAvatar
+          username={row.original.username}
+          imageUrl={row.original.imageUrl}
+        />
+        <span>{row.original.username}</span>
+      </div>
+    ),
+  },
+  {
+    accessorKey: "createdAt",
+    header: "Date blocked",
+  },
+  {
+    id: "actions",
+    cell: () => <Button>Unblock</Button>,
+  },
+];
