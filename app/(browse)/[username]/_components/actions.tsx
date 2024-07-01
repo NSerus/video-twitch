c4e75c7cee6761f20632ec8e626c0d21705904ef
NSerus <nunoworkingserus@gmail.com>
2024-07-01 20:58:24 +0100

    bugfix

--- app/(browse)/[username]/_components/actions.tsx
+++ app/(browse)/[username]/_components/actions.tsx
@@ -46,7 +46,7 @@ export function Actions({ isFollowing, userId }: ActionsProps) {
   const handleBlock = () => {
     startTransition(() => {
       onBlock(userId)
-        .then((data) => toast.success(`Blocked user ${data.blocked.username}`))
+        .then((data) => toast.success(`Blocked user ${data?.blocked.username}`))
         .catch(() => toast.error("Something went wrong"));
     });
   };
